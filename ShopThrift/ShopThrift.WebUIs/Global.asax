32cb7da219e64ba7945d5cb9c9df56d034df19d2
Devon Gaston <kendgast@ut.utm.edu>
2020-01-24 19:37:10 -0600

    Initial solution setup.

---
+++ ShopThrift/ShopThrift.WebUIs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopThrift.WebUIs.MvcApplication" Language="C#" %>
---
+++ ShopThrift/ShopThrift.WebUIs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopThrift.WebUIs.Startup))]
+namespace ShopThrift.WebUIs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
